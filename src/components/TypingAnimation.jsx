08b8bcc0774b5dace3b9fcb2e0be158dcf336270
rajakokate <rajakokate420@gmail.com>
2024-10-02 22:17:40 +0530

    added typing effect, fixed overscroll

--- src/components/TypingAnimation.jsx
+++ src/components/TypingAnimation.jsx
@@ -3,12 +3,20 @@ import { Typewriter } from "react-simple-typewriter";
 
 const TypingAnimation = () => {
   return (
-    <div style={{ fontSize: "24px", color: "black", backgroundColor:"white", border:"1px solid black", borderRadius:"10px ", padding:"20px", zIndex:"5555555" }}>
+    <div
+      style={{
+        fontSize: "24px",
+        color: "black",
+        backgroundColor: "white",
+        border: "1px solid black",
+        borderRadius: "10px ",
+        padding: "20px",
+        zIndex: "5555555",
+      }}
+    >
       <Typewriter
-        words={[
-          "Delivered fresh, just like home. Sign up or log in to start ordering!",
-        ]}
-        loop={1}
+        words={["स्वाद और सेहत से भरपूर घर जैसा खाना, आपके दरवाज़े तक!"]}
+        loop={5}
         cursor
         cursorStyle="_"
         typeSpeed={70}
